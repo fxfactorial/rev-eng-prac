1a82ce3fc623740336d5e383a8619eb7ad7db843
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-11-29 09:34:57 -0800

    Initial Correct repo for ios_arm and osx_64

---
+++ osx_x64/rev_eng.c
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+#include <mach-o/arch.h>
+#include <mach-o/loader.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello World\n");
+  return 0;
+}
